46bb47aac710563691e0dd739f5c021bdf161e70
elenabor2003@gmail.com <elenabor2003@gmail.com>
2023-01-13 17:09:14 +0300

    Добавьте файлы проекта.

---
+++ PR15/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR15
+{
+    public static class Model
+    {
+        // блок с данными
+
+    }
+}
