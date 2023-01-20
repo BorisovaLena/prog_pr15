0c44aa2994136abe8bfd9c5bba732c2b7b10612b
elenabor2003@gmail.com <elenabor2003@gmail.com>
2023-01-20 17:04:20 +0300

    итог

--- PR15/MainWindow.xaml.cs
+++ PR15/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -35,25 +36,41 @@ namespace PR15
 
         private void btnCalc_Click(object sender, RoutedEventArgs e)
         {
-            int a = Convert.ToInt32(tbOne.Text);
-            int b = Convert.ToInt32(tbTwo.Text);
-            int c =0;
-            switch(cmbArifm.SelectedIndex)
+            if(tbOne.Text==""|| tbTwo.Text=="")
             {
-                case 0:
-                    c = a + b;
-                    break;
-                case 1:
-                    c = a - b;
-                    break;
-                case 2:
-                    c = a * b;
-                    break;
-                case 3:
-                    c = a / b;
-                    break;
+                MessageBox.Show("Есть незаполненные поля!!!");
             }
-            Model.Calculation = c;
+            else
+            {
+                Regex r1 = new Regex("^[0-9]+$");
+                if (r1.IsMatch(tbOne.Text)&& r1.IsMatch(tbTwo.Text))
+                {
+                    int a = Convert.ToInt32(tbOne.Text);
+                    int b = Convert.ToInt32(tbTwo.Text);
+                    int c = 0;
+                    switch (cmbArifm.SelectedIndex)
+                    {
+                        case 0:
+                            c = a + b;
+                            break;
+                        case 1:
+                            c = a - b;
+                            break;
+                        case 2:
+                            c = a * b;
+                            break;
+                        case 3:
+                            c = a / b;
+                            break;
+                    }
+                    Model.Calculation = c;
+                }
+                else
+                {
+                    MessageBox.Show("Введены данные неправильного формата!!! Введите только натуральные числа!!!");
+                }
+               
+            }
         }
     }
 }
